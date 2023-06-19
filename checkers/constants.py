c5636c01656464ab7063065fa23fab4733c6c06f
mrevanzak <mrevanzak@gmail.com>
2023-06-19 12:56:54 +0700

    done

---
+++ checkers/constants.py
@@ -0,0 +1,17 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS = 8
+COLS = ROWS
+SQUARE_SIZE = WIDTH // COLS
+
+# rgb
+RED = (246, 69, 77)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (70, 198, 247)
+GREY = (128, 128, 128)
+BLUE = (0, 0, 255)
+ORANGE = (255, 147, 82)
+
+CROWN = pygame.transform.scale(pygame.image.load("assets/crown.png"), (44, 25))
---
+++ main.py
@@ -0,0 +1,118 @@
+import pygame
+from checkers.button import Button
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, BLUE
+from checkers.game import Game
+from minimax.algorithm import minimax
+
+FPS = 60
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Checkers")
+font = "assets/big_noodle_titling.ttf"
+BG = pygame.image.load("assets/bg.jpeg")
+
+def get_font(size):
+    return pygame.font.Font(font, size)
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
+
+def main(mode):
+    run = True
+    clock = pygame.time.Clock()
+    game = Game(SCREEN)
+
+    while run:
+        clock.tick(FPS)
+
+        if game.turn == BLUE and mode == "pvc":
+            value, new_board = minimax(game.get_board(), 2, BLUE, game)
+            game.ai_move(new_board)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    main_menu()  # Return to the main menu
+
+        game.update()
+
+    pygame.quit()
+
+def main_menu():
+    menu = True
+    clock = pygame.time.Clock()
+
+    while menu:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("CHECKERS", True, "#9267ff")
+        MENU_RECT = MENU_TEXT.get_rect(center=(400, 200))
+
+        PLAY_BUTTON = Button(
+            image=pygame.image.load("assets/Button Rect.png"),
+            pos=(400, 350),
+            text_input="PvP",
+            font=get_font(75),
+            base_color="#d7fcd4",
+            hovering_color="White",
+        )
+        OPTIONS_BUTTON = Button(
+            image=pygame.image.load("assets/Button Rect.png"),
+            pos=(400, 475),
+            text_input="PvC",
+            font=get_font(75),
+            base_color="#d7fcd4",
+            hovering_color="White",
+        )
+        QUIT_BUTTON = Button(
+            image=pygame.image.load("assets/Button Rect.png"),
+            pos=(400, 600),
+            text_input="QUIT",
+            font=get_font(75),
+            base_color="#d7fcd4",
+            hovering_color="White",
+        )
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menu = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    main("pvp")
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    main("pvc")
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    menu = False
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+    pygame.quit()
+
+main_menu()
